d8831cf4507ee2de1f03badbb7abaa7d77f01fc9
sirPinwheel <pinwheelgm+git@gmail.com>
2020-02-03 22:01:03 +0100

    update

--- main.py
+++ main.py
@@ -23,38 +23,40 @@ def draw(source, start_x, start_y) -> None:
         for x in range(pic.size[0]):
             pixel = (x, pic.size[1] - y -1)
             cords = (x + start_x, y + start_y)
-            c = None
 
             if bands == 'rgb':
                 r, g, b = pic.getpixel(pixel)
-                if OMMIT_WHITE:
-                    if r > OMMIT_TRESHOLD and g > OMMIT_TRESHOLD and b > OMMIT_TRESHOLD:
-                        continue
+                if OMMIT_WHITE and r > OMMIT_TRESHOLD and g > OMMIT_TRESHOLD and b > OMMIT_TRESHOLD: continue
 
             elif bands == 'rgba':
                 if ALPHA_BLENDING:
                     src_r, src_g, src_b, src_a = pic.getpixel(pixel)
                     dst_r, dst_g, dst_b = BLENDING_COLOR
+                    src_a = src_a / 255
 
-                    r = (src_r * src_a) + (dst_r * (1 - src_a))
-                    g = (src_g * src_a) + (dst_g * (1 - src_a))
-                    b = (src_b * src_a) + (dst_b * (1 - src_a))
+                    r = int((src_r * src_a) + (dst_r * (1 - src_a)))
+                    g = int((src_g * src_a) + (dst_g * (1 - src_a)))
+                    b = int((src_b * src_a) + (dst_b * (1 - src_a)))
+
+                    if OMMIT_WHITE and r > OMMIT_TRESHOLD and g > OMMIT_TRESHOLD and b > OMMIT_TRESHOLD: continue
                 else:
                     r, g, b, a = pic.getpixel(pixel)
                     if a < 15: continue
-                    elif OMMIT_WHITE:
-                        if r > OMMIT_TRESHOLD and g > OMMIT_TRESHOLD and b > OMMIT_TRESHOLD:
-                            continue
+                    if OMMIT_WHITE and r > OMMIT_TRESHOLD and g > OMMIT_TRESHOLD and b > OMMIT_TRESHOLD: continue
 
             elif bands == 'bw':
                 c = pic.getpixel(pixel)
-                if c == 255 and OMMIT_WHITE:
-                    continue
 
-            if c != None:
-                if c == 0: color = '000000'
-                else: color = 'ffffff'
-            else: color = '{:02x}{:02x}{:02x}'.format(r, g, b)
+                if OMMIT_WHITE:
+                    if c == 255: continue
+                    else: r, g, b = (0, 0, 0)
+                else:
+                    if c == 255: r, g, b = (255, 255, 255)
+                    else: r, g, b = (0, 0, 0)
+
+            else: raise RuntimeError("unsupported color band format")
+
+            color = '{:02x}{:02x}{:02x}'.format(r, g, b)
 
             irc().send_message(f'!{str(cords[0])} {str(cords[1])} #{color}')
             sleep(DELAY)
